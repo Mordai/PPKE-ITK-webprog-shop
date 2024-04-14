abcc2309f21a9da2679a49ef55374fc1c9f0038e
Lengyel Balázs Ádám <lengyel.balazs.adam@hallgato.ppke.hu>
2024-04-15 00:09:29 +0200

    added not products found label

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard.tsx";
 import { ProductData, apiURL } from "./Types.ts";
+import { Typography } from "@mui/material";
 
 type HomeProps = {
   addToBasket: (product: ProductData) => void;
@@ -18,13 +19,17 @@ export default function Home(props: HomeProps) {
 
   return (
     <>
-      {products.map((product) => (
-        <ProductCard
-          key={product.id}
-          product={product}
-          addToBasket={props.addToBasket}
-        />
-      ))}
+      {products.length > 0 ? (
+        products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            addToBasket={props.addToBasket}
+          />
+        ))
+      ) : (
+        <Typography variant="h6">Nincs megjeleníthető termék.</Typography>
+      )}
     </>
   );
 }
